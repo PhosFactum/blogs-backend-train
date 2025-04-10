4e90ba5d7a4e22de900d6133f80fb54de6e0c89d
PhosFactum <phosphorus.work@gmail.com>
2025-04-10 09:22:09 +0300

    Paths and hinting

--- main.py
+++ main.py
@@ -4,10 +4,25 @@ app = FastAPI()
 
 
 @app.get("/")
-def baseFunc():
-    return {"data": {"name": "Mike"}}
+def get_root():
+    return {"data": "blog list"}
 
 
 @app.get("/about")
-def about():
+def get_about():
     return {"data": "about page"}
+
+
+@app.get("/page/unpublished")
+def get_unpublished():
+    return {"data": "all unpublished pages"}
+
+
+@app.get("/page/{id}")
+def get_page(id: int):
+    return {"data": id}
+
+
+@app.get("/page/{id}/info")
+def get_page_info(id):
+    return {"data": {"1", "2"}}
