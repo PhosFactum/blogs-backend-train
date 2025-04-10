ac7538721ee0e5576556c4700c0d814deaff7072
PhosFactum <phosphorus.work@gmail.com>
2025-04-10 09:55:26 +0300

    Post and Swagger API checking

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -15,7 +16,6 @@ def get_page(limit=10, published: bool = True, sort: Optional[str] = None):
         return {"data": f"{limit} published pages from the db"}
     else:
         return {"data": f"{limit} pages from the db"}
-
 
 
 @app.get("/about")
@@ -36,3 +36,14 @@ def get_page(id: int):
 @app.get("/page/{id}/info")
 def get_page_info(id, limit=10):
     return {"data": {"1", "2"}}
+
+
+class Page(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post("/page")
+def create_page(page: Page):
+    return {"data": f"Page is created with title as {page.title}"}
