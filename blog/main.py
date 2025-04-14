ec337c95d96f0292793eb9b05a3311220404131c
PhosFactum <phosphorus.work@gmail.com>
2025-04-14 10:07:11 +0300

    Feat: put, delete requests

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 import schemas, models
@@ -17,10 +17,51 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+def post_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {"done"}
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id:int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+
+    blog.update(request)
+    db.commit()
+    return "updated"
+
+
+@app.get("/blog")
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}", status_code=200)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id} is not available"
+        )
+    return blog
