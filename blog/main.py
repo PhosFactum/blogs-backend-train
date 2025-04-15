cc056ad243f321faf932fafdec91301a25fcf369
PhosFactum <phosphorus.work@gmail.com>
2025-04-15 16:04:45 +0300

    Feat: /user GET and POST handlers

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,8 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+
 import schemas, models
 
 
@@ -16,7 +18,9 @@ def get_db():
         db.close()
 
 
-# Endpoints
+### Endpoints ###
+
+# Blog handlers
 @app.post("/blog", status_code=status.HTTP_201_CREATED)
 def post_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
@@ -50,7 +54,7 @@ def update_blog(id:int, request: schemas.Blog, db: Session = Depends(get_db)):
     return "updated"
 
 
-@app.get("/blog", response_model=schemas.ShowBlog)
+@app.get("/blog", response_model=List[schemas.ShowBlog])
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
@@ -65,3 +69,19 @@ def get_blog(id: int, db: Session = Depends(get_db)):
             detail=f"Blog with id {id} is not available"
         )
     return blog
+
+
+# Users handlers
+@app.post("/user")
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/user", response_model=List[schemas.ShowBlog])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
--- blog/schemas.py
+++ blog/schemas.py
@@ -5,6 +5,18 @@ class Blog(BaseModel):
     title: str
     body: str
 
+
 class ShowBlog(Blog):
+    class Config():
+        orm_mode = True
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class ShowUser(User):
     class Config():
         orm_mode = True
