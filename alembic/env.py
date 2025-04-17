763c83c35d02db4c4aaa5c90f2679405fecc0775
PhosFactum <phosphorus.work@gmail.com>
2025-04-17 19:58:34 +0300

    fix(alembic): working rightly

--- alembic/env.py
+++ alembic/env.py
@@ -1,15 +1,16 @@
 from logging.config import fileConfig
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
-from database import Base
-import models
+from src.database import Base
+from src import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
